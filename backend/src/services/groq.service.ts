e91efff9d3c6479ac4c2390107e6b7d153e50798
danilowskii <danilopaivatiotonho@gmail.com>
2025-11-24 16:55:40 -0300

    fix, error bad request 400 backend. switch fs promises from node to toFile (helper) from groq, then every chunk will have the necessary  metadata with correctly extension .webm. Fix on groq.service.ts

--- backend/src/services/groq.service.ts
+++ backend/src/services/groq.service.ts
@@ -1,7 +1,7 @@
 import path from "node:path";
 import fs from "node:fs";
 import { file as tmpFile } from "tmp-promise";
-import Groq from "groq-sdk";
+import Groq, { toFile } from "groq-sdk";
 
 import "dotenv/config";
 
@@ -11,44 +11,49 @@ export const groqPartialTranscription = async (
   webmBuffer: Buffer
 ): Promise<string> => {
   // cria arquivo temporário
-  const { path: tmpPath, cleanup } = await tmpFile({ postfix: ".webm" });
+
   try {
+    //const { path: tmpPath, cleanup } = await tmpFile({ postfix: ".webm" });
+    // ===== usando toFile aqui para enviar com segurança sem faltar os headers que groq pede
+    const file = await toFile(webmBuffer, "partial-audio.webm");
     // grava o buffer no arquivo temporário
-    await fs.promises.writeFile(tmpPath, webmBuffer);
+    //await fs.promises.writeFile(tmpPath, webmBuffer);
 
     // cria readstream
-    const stream = fs.createReadStream(tmpPath);
+    //const stream = fs.createReadStream(tmpPath);
 
     const response = await groq.audio.transcriptions.create({
-      file: stream,
+      file: file,
       model: "whisper-large-v3-turbo",
       response_format: "json",
       language: "pt",
+      temperature: 0.0,
     });
 
     return response.text ?? "";
-  } finally {
-    // limpa arquivo temporário
-    await cleanup();
+  } catch (error) {
+    console.error("Erro Groq Partial: ", error);
+    return "";
   }
 };
 export const groqFullTranscription = async (
   webmBuffer: Buffer
 ): Promise<string> => {
-  const { path: tmpPath, cleanup } = await tmpFile({ postfix: ".webm" });
-
   try {
-    await fs.promises.writeFile(tmpPath, webmBuffer);
-
-    const stream = fs.createReadStream(tmpPath);
+    //const { path: tmpPath, cleanup } = await tmpFile({ postfix: ".webm" });
+    //await fs.promises.writeFile(tmpPath, webmBuffer);
+    const file = await toFile(webmBuffer, "full-audio.webm");
+    //const stream = fs.createReadStream(tmpPath);
 
     const response = await groq.audio.transcriptions.create({
-      file: stream,
+      file: file,
       model: "whisper-large-v3-turbo",
       response_format: "json",
+      language: "pt",
     });
     return response.text ?? "";
-  } finally {
-    await cleanup();
+  } catch (error) {
+    console.error("Erro Groq Full:", error);
+    throw error;
   }
 };
